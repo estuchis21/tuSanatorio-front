970ff078c788a87ff3314a29aaca28ab1108389a
albano martinez <albano_107@hotmail.com>
2025-06-23 16:27:41 -0300

    Front completo

--- src/diseno/DiseñoMedico.jsx
+++ src/diseno/DiseñoMedico.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "../estilos/Layout.css";
 
-export default function DiseñoMedico({ children }) {
+export default function DiseñoMedico() {
   return (
     <div className="layout">
       <header className="layout-header">
         <h1>Portal del Médico</h1>
         <nav>
-          <Link to="/home">Inicio</Link>
-          <Link to="/medico">Pacientes</Link>
+          <Link to="/medico/turnos">Turnos del Día</Link>
           <Link to="/">Cerrar sesión</Link>
         </nav>
       </header>
       <main className="layout-contenido">
-        {children}
+        <Outlet />
       </main>
     </div>
   );
 }
+
